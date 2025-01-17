7d0e175f0a1079ee8a1bc07da2f86b44002b311b
快醒醒你只是条单身汪 <langbiantianya@outlook.com>
2025-01-17 03:43:27 +0000

    fix: config

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -2,6 +2,25 @@ import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
+  site: {
+    title: '博客',
+    subtitle: 'Lanbiantianya',
+    author: 'Lanbiantianya',
+    description: '',
+    website: 'https://blog.kxxnzstdsw.com/',
+    socialLinks: [{
+      name: 'github',
+      href: 'https://github.com/langbiantianya',
+    }, {
+      name: 'rss',
+      href: '/atom.xml',
+    }],
+    categoryMap: [],
+    footer: [
+      '© %year <a target="_blank" href="%website">%author</a>',
+      'Theme <a target="_blank" href="https://github.com/Moeyua/astro-theme-typography">Typography</a> by <a target="_blank" href="https://moeyua.com">Moeyua</a>',
+      'Proudly published with <a target="_blank" href="https://astro.build/">Astro</a>',
+    ],
+  },
+
 }
