e1b7f4f0dcf7f50cbd53a54818a142d90b75ef15
lbty <langbiantianya@outlook.com>
2025-09-24 14:17:20 +0000

    feat: 修改个人信息

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "langbiantianya",
+	bio: "愿此行终抵群星",
 	links: [
 		// {
 		// 	name: "Twitter",
@@ -78,7 +78,7 @@ export const profileConfig: ProfileConfig = {
 };
 
 export const licenseConfig: LicenseConfig = {
-	enable: true,
+	enable: false,
 	name: "CC BY-NC-SA 4.0",
 	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
 };
